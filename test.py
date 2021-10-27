8bdb87ec3f93448eca5621ec2792ed663c7c3918
JoeWang <s955607@gmail.com>
2021-10-27 14:41:03 +0800

    delete unused file

--- test.py
+++
@@ -1,5 +0,0 @@
-#%%
-url = 'https://www.ybe.com/watch?v=pkbqFhlQFbc&list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbAVMpkbqFhlQFbc&start_radio=1&ab_channel=%E7%90%86%E6%83%B3%E6%B7%B7%E8%9B%8B-%E9%9B%9E%E4%B8%81'
-
-print(url.find('youtu'))
-# %%
